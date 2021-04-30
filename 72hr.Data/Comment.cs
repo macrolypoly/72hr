ed46eb6d705b3f3c87c87933f9a1f3848bf5e415
macrolypoly <krodriguez61@ivytech.edu>
2021-04-30 18:12:42 -0400

    added classes

---
+++ 72hr.Data/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _72hr.Data
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        Guid AuthorId { get; set; }
+        public virtual List<Reply> Replies { get; set; }
+        [ForeignKey("PostId")]
+        public int PostId { get; set; }
+        public virtual Post Post { get; set; }
+    }
+}
